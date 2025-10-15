13f607a1a3a0128dd6b1221e832ecb9e57a42e1e
Max Sergienko <azzaro.max@gmail.com>
2025-10-15 10:45:20 +0300

    dataset by id

--- dataset.py
+++ dataset.py
@@ -3,17 +3,18 @@ import requests
 import io
 
 
-DATASET_URl="https://raw.githubusercontent.com/maksim-sergienko/data-source/refs/heads/main/jupyter/3.csv"
+DATASET_ID=3    #here put dataset ID
+
+DATASET_URl="https://raw.githubusercontent.com/maksim-sergienko/data-source/refs/heads/main/jupyter/" + DATASET_ID +  ".csv"
 
 def render_csv_from_url(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad status codes
+        response.raise_for_status()
         csv_data = response.content
         df = pd.read_csv(io.StringIO(csv_data.decode('utf-8')))
 
-        df.head()
-
+        display(df)
 
     except requests.exceptions.RequestException as e:
         print(f"Error downloading the CSV file: {e}")
