41c07b01c533491aee5f64852819e1538592bea4
Max Sergienko <azzaro.max@gmail.com>
2025-11-04 14:36:21 +0200

    add plotly

--- template/template.py
+++ template/template.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import io
+import plotly.express as px
 
 
 print("Notebook for: {{DATASET_NAME}} ({{DATASET_ID}})")
@@ -9,7 +10,7 @@ print("Description: {{DESCRIPTION}}")
 DATASET_URl="{{DATASET_URL}}"
 
 
-def render_csv_from_url(url):
+def render_csv_from_url(url, x_col=None, y_col=None):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -17,6 +18,18 @@ def render_csv_from_url(url):
         df = pd.read_csv(io.StringIO(csv_data.decode('utf-8')))
 
         display(df)
+
+        if x_col and y_col:
+            if x_col in df.columns and y_col in df.columns:
+                print(f"Generating a scatter plot with x='{x_col}' and y='{y_col}'...")
+                fig = px.scatter(df, x=x_col, y=y_col)
+                fig.show()
+            else:
+                print(f"Error: One or both specified columns ('{x_col}', '{y_col}') not in dataframe.")
+        elif df.shape[1] >= 2:
+            print("Generating a scatter plot with the first two columns...")
+            fig = px.scatter(df, x=df.columns[0], y=df.columns[1])
+            fig.show()
 
     except requests.exceptions.RequestException as e:
         print(f"Error downloading the CSV file: {e}")
